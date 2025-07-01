eb65ea9d0612c21cc950bc8b718ba97bd342d459
AMCh99 <alek123252@gmail.com>
2025-07-01 22:59:01 +0200

    add: transition to CardContent

--- src/components/home/movieCard.tsx
+++ src/components/home/movieCard.tsx
@@ -27,9 +27,7 @@ export function MovieCard(props: Props) {
         <Grid
             item
             onClick={() =>
-                router.push(
-                    `/${movie.media_type ?? media_type}/${movie.id}`
-                )
+                router.push(`/${movie.media_type ?? media_type}/${movie.id}`)
             }
         >
             <Card raised sx={{ width: '200px', margin: '5px' }}>
@@ -38,7 +36,7 @@ export function MovieCard(props: Props) {
                         backgroundImage: `url(https://image.tmdb.org/t/p/original${movie?.poster_path})`,
                         height: '300px',
                         backgroundSize: 'cover',
-                        backgroundPosition: 'center',
+                        backgroundPosition: 'center'
                     }}
                     onMouseEnter={() => {
                         setIsMouseOver(true);
@@ -47,39 +45,39 @@ export function MovieCard(props: Props) {
                         setIsMouseOver(false);
                     }}
                 >
-                    {isMouseOver && (
-                        <CardContent
+                    <CardContent
+                        sx={{
+                            backgroundImage: `linear-gradient(to bottom, rgba(0,0,0, 0.0), rgba(106, 166, 177,0.9) 70%)`,
+                            height: '100%',
+                            alignContent: 'end',
+                            opacity: isMouseOver ? 1 : 0,
+                            transition: 'opacity 0.2s ease-in-out'
+                        }}
+                    >
+                        <Typography variant="subtitle2" align="right">
+                            {movie.title ?? movie?.name}
+                        </Typography>
+                        <Box
                             sx={{
-                                backgroundImage: `linear-gradient(to bottom, rgba(0,0,0, 0.0), rgba(106, 166, 177,0.9) 70%)`,
-                                height: '100%',
-                                alignContent: 'end'
+                                display: 'flex',
+                                height: 'min-content',
+                                float: 'right'
                             }}
                         >
-                            <Typography variant='subtitle2' align='right'>
-                                {movie.title ?? movie?.name}
-                            </Typography>
-                            <Box
-                                sx={{
-                                    display: 'flex',
-                                    height: 'min-content',
-                                    float: 'right'
-                                }}
-                            >
-                                <CardActions sx={{ p: 0 }}>
-                                    <IconButton>
-                                        <WatchLaterIcon />
-                                    </IconButton>
-                                </CardActions>
-                                <Rating
-                                    name='read-only'
-                                    value={(movie?.vote_average ?? 0) / 2}
-                                    readOnly
-                                    precision={0.1}
-                                    sx={{ alignSelf: 'center', p: 0 }}
-                                />
-                            </Box>
-                        </CardContent>
-                    )}
+                            <CardActions sx={{ p: 0 }}>
+                                <IconButton>
+                                    <WatchLaterIcon />
+                                </IconButton>
+                            </CardActions>
+                            <Rating
+                                name="read-only"
+                                value={(movie?.vote_average ?? 0) / 2}
+                                readOnly
+                                precision={0.1}
+                                sx={{ alignSelf: 'center', p: 0 }}
+                            />
+                        </Box>
+                    </CardContent>
                 </Box>
             </Card>
         </Grid>
